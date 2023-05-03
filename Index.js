4c150dee47b159c9f352e9d8892aa977ddc84251
matuspytel <132471479+matuspytel@users.noreply.github.com>
2023-05-03 16:31:20 +0200

    Delete Index.js

--- Index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#next").addEventListener("click", function () {
-    window.location.href = "atributes.html";
-})
-document.querySelector("#next-controll").addEventListener("click", function () {
-    window.location.href = "atributes.html";
-})
