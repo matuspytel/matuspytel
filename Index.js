bb742126f405b1a4b9a0b25cddbeb1872fcb87ad
matuspytel <132471479+matuspytel@users.noreply.github.com>
2023-05-03 16:22:46 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,6 @@
+document.querySelector("#next").addEventListener("click", function () {
+    window.location.href = "atributes.html";
+})
+document.querySelector("#next-controll").addEventListener("click", function () {
+    window.location.href = "atributes.html";
+})
